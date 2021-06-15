b804ceb7abf7708d999433577887370fbf450d5e
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-15 00:50:24 -0400

    fixed bug, server.js was in the wrong location was casuing the error

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,14 +1,15 @@
 const tableNotes = require('../../db/db.json');
+const {nanoid} = require('nanoid');
+const fs = require('fs')
 
 module.exports = (app) => {
     
   
-    app.get('/api/notes', (req, res) => res.json(tableNotes));  
- 
-  
-    app.post('/api/notes', (req, res) => {
-
-      
+    // app.get('/api/notes', (req, res) => res.json(tableNotes));  
+    app.get('/api/notes', (req,res) => {
+      res.json(tableNotes);
+    });
+    app.post('/api/notes', (req, res) => {      
                 res.json(true);
      
       }
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./public/routes/apiRoutes')(app);
+require('./public/routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on localhosePORT: localhost:${PORT}`);
