a5702c42c126b869ebbdd1284d58ebfbd50e4428
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-14 23:47:53 -0400

    html router works, had to fix typo in path

--- public/assets/js/routes/htmlRoutes.js
+++ public/assets/js/routes/htmlRoutes.js
@@ -6,15 +6,15 @@ module.exports = (app) => {
     // In each of the below cases the user is shown an HTML page of content
   
     app.get('/index', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../index.html'));
+      res.sendFile(path.join(__dirname, '../../../index.html'));
     });
   
     app.get('/notes', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../notes.html'));
+      res.sendFile(path.join(__dirname, '../../../notes.html'));
     });
   
     // If no matching route is found default to home
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../index.html'));
+      res.sendFile(path.join(__dirname, '../../../index.html'));
     });
   };
