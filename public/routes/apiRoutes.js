508643effe01953a063dfba9af95d0cec60f7b3a
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-15 02:05:34 -0400

    got the path to update db.json right

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -9,13 +9,24 @@ module.exports = (app) => {
     app.get('/api/notes', (req,res) => {
       res.json(tableNotes);
     });
-    app.post('/api/notes', (req, res) => {      
-                res.json(true);
+    // app.post('/api/notes', (req, res) => {      
+    //             res.json(true);
      
-      }
-    );
-  
+    //   }
+    // );
+   
+    app.post('/api/notes', (req, res) => {
+      console.log(tableNotes);
+      console.log(req.body);
+      req.body.id = nanoid(10);
+      tableNotes.push(req.body);
+      console.log(tableNotes);
+      updateNotes(tableNotes)
+      res.json(tableNotes)
+    })
+  };
 
-  
-
+  updateNotes = () => {
+    fs.writeFile('./db/db.json', JSON.stringify(tableNotes),
+    (err) => err ? console.log("Somethings wrong!", err) : console.log(`db updated`))
   };
